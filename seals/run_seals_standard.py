7cfbc298e5c29a148157d2d423572ceb59391700
barbarazimbres <barbara.zimbres@ipam.org.br>
2025-06-17 10:45:43 -0300

    Adapted for the Brazil mapbiomas run

--- seals/run_seals_standard.py
+++ seals/run_seals_standard.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # files that already exist.
     p.user_dir = os.path.expanduser('~')
     p.extra_dirs = ['Files', 'seals', 'projects']
-    p.project_name = 'standard'
+    p.project_name = 'Brazil_magpie_mapbiomas'
     p.project_name = p.project_name + '_' + hb.pretty_time() # If don't you want to recreate everything each time, comment out this line.
 
     # Based on the paths above, set the project_dir. All files will be created in this directory.
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     # If you have not run SEALS before, SEALS will generate it in your project's input_dir.
     # A useful way to get started is to to run SEALS on the test data without modification
     # and then edit the scenario_definitions.csv to your project needs.   
-    p.scenario_definitions_filename = 'standard_scenarios.csv' 
+    p.scenario_definitions_filename = 'standard_scenarios_BRA_magpie_mapbiomas2.csv' 
     p.scenario_definitions_path = os.path.join(p.input_dir, p.scenario_definitions_filename)
     seals_initialize_project.initialize_scenario_definitions(p)
         
